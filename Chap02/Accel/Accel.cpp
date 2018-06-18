5acbe21f9f4eac6fe196ee335d5189c11ecca8f9
LiShulian <shuyan2@126.com>
2018-06-18 23:26:16 +0800

    update change

--- Chap02/Accel/Accel.cpp
+++ Chap02/Accel/Accel.cpp
@@ -8,9 +8,6 @@ CMyApp myApp;
 
 BOOL CMyApp::InitInstance ()
 {
-    m_pMainWnd->ShowWindow (m_nCmdShow);
-    m_pMainWnd->UpdateWindow ();
-    return TRUE;
 	m_pMainWnd = new CMainWindow;
 	m_pMainWnd->ShowWindow(m_nCmdShow);
 	m_pMainWnd->UpdateWindow();
