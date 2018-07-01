71a4d26111af8a10e1614d21300a404e255cfc31
LiShulian <shuyan2@126.com>
2018-07-01 22:14:39 +0800

    modify

--- Chap08/Phones/ChildView.cpp
+++ Chap08/Phones/ChildView.cpp
@@ -16,7 +16,7 @@ static char THIS_FILE[] = __FILE__;
 // CChildView
 
 const TCHAR CChildView::m_szFilters[] =
-    _T ("Phone Files (*.phn)|*.phn|All Files (*.*)|*.*||");
+_T("Phone Files (*.phn)|*.phn|All Files (*.*)|*.*||");
 
 CChildView::CChildView()
 {
@@ -38,7 +38,7 @@ END_MESSAGE_MAP()
 /////////////////////////////////////////////////////////////////////////////
 // CChildView message handlers
 
-BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs) 
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs)
 {
 	if (!CPhonesListBox::PreCreateWindow(cs))
 		return FALSE;
@@ -48,89 +48,88 @@ BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs)
 	return TRUE;
 }
 
-void CChildView::OnNewEntry() 
+void CChildView::OnNewEntry()
 {
-	NewEntry ();	
+	NewEntry();
 }
 
-void CChildView::OnFileOpen() 
+void CChildView::OnFileOpen()
 {
-    CFileDialog dlg (TRUE, _T ("phn"), _T ("*.phn"),
-        OFN_FILEMUSTEXIST | OFN_HIDEREADONLY, m_szFilters);
+	CFileDialog dlg(TRUE, _T("phn"), _T("*.phn"),
+		OFN_FILEMUSTEXIST | OFN_HIDEREADONLY, m_szFilters);
 
-    if (dlg.DoModal () == IDOK) {
-        if (LoadFile (dlg.GetPathName ())) {
-            m_strPathName = dlg.GetPathName ();
-			SetCurSel (0);
+	if (dlg.DoModal() == IDOK) {
+		if (LoadFile(dlg.GetPathName())) {
+			m_strPathName = dlg.GetPathName();
+			SetCurSel(0);
 		}
 	}
 }
 
-void CChildView::OnFileSave() 
+void CChildView::OnFileSave()
 {
-    if (!m_strPathName.IsEmpty ())
-        SaveFile (m_strPathName);
-    else // Need a file name first
-        OnFileSaveAs ();
+	if (!m_strPathName.IsEmpty())
+		SaveFile(m_strPathName);
+	else // Need a file name first
+		OnFileSaveAs();
 }
 
-void CChildView::OnFileSaveAs() 
+void CChildView::OnFileSaveAs()
 {
-    CFileDialog dlg (FALSE, _T ("phn"), m_strPathName,
-        OFN_OVERWRITEPROMPT | OFN_PATHMUSTEXIST | OFN_HIDEREADONLY,
-        m_szFilters);
+	CFileDialog dlg(FALSE, _T("phn"), m_strPathName,
+		OFN_OVERWRITEPROMPT | OFN_PATHMUSTEXIST | OFN_HIDEREADONLY,
+		m_szFilters);
 
-    if (dlg.DoModal () == IDOK)
-        if (SaveFile (dlg.GetPathName ()))
-            m_strPathName = dlg.GetPathName ();
+	if (dlg.DoModal() == IDOK)
+		if (SaveFile(dlg.GetPathName()))
+			m_strPathName = dlg.GetPathName();
 }
 
 BOOL CChildView::LoadFile(LPCTSTR pszFile)
 {
-    BOOL bResult = FALSE;
+	BOOL bResult = FALSE;
 
 	try {
-		CStdioFile file (pszFile, CFile::modeRead);
-		ResetContent ();
-		DWORD dwCount;
-		file.Read (&dwCount, sizeof (dwCount));
-		if (dwCount) {
-			for (int i=0; i<(int) dwCount; i++) {
-				CString string;
-				file.ReadString (string);
-				AddString (string);
-			}
+		CStdioFile file(pszFile, CFile::modeRead);
+		ResetContent();
+		DWORD dwCount = 0;
+		file.Read(&dwCount, sizeof(dwCount));
+
+		for (int i = 0; i < (int)dwCount; i++) {
+			CString string;
+			file.ReadString(string);
+			AddString(string);
 		}
 		bResult = TRUE;
 	}
 	catch (CFileException* e) {
-		e->ReportError ();
-		e->Delete ();
+		e->ReportError();
+		e->Delete();
 	}
-    return bResult;
+	return bResult;
 }
 
 BOOL CChildView::SaveFile(LPCTSTR pszFile)
 {
-    BOOL bResult = FALSE;
+	BOOL bResult = FALSE;
 
 	try {
-		CStdioFile file (pszFile, CFile::modeWrite | CFile::modeCreate);
-		DWORD dwCount = GetCount ();
-		file.Write (&dwCount, sizeof (dwCount));
+		CStdioFile file(pszFile, CFile::modeWrite | CFile::modeCreate);
+		DWORD dwCount = GetCount();
+		file.Write(&dwCount, sizeof(dwCount));
 		if (dwCount) {
-			for (int i=0; i<(int) dwCount; i++) {
+			for (int i = 0; i < (int)dwCount; i++) {
 				CString string;
-				GetText (i, string);
-				string += _T ("\n");
-				file.WriteString (string);
+				GetText(i, string);
+				string += _T("\n");
+				file.WriteString(string);
 			}
 		}
 		bResult = TRUE;
 	}
 	catch (CFileException* e) {
-		e->ReportError ();
-		e->Delete ();
+		e->ReportError();
+		e->Delete();
 	}
-    return bResult;
+	return bResult;
 }
